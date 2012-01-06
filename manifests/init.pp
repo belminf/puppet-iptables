306e062e8878e297cacb1ddaec77052931a6df3f
Belmin Fernandez <belminf@gmail.com>
2012-01-05 21:37:14 -0500

    added dns rule and define function

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,5 @@
+# Make sure we run this before anything
+stage { pre: before => Stage[main] }
 class { 'iptables': stage => 'pre' }
 
 class iptables {
@@ -6,16 +8,32 @@ class iptables {
 	       purge => true,
 	}
 
-	firewall { '000 in: accept local':
+	firewall { '000 input: accept local':
 		chain => 'INPUT',
 		iniface => 'lo',
 		action => 'accept',
 	}
 
-	firewall { '000 in: accept related and established':
+	firewall { '000 input: accept related and established':
 		chain => 'INPUT',
 		state => ['ESTABLISHED', 'RELATED'],
 		action => 'accept',
 	}
-	
+
+	firewall { '000 input: accept dns':
+		chain => 'INPUT',
+		proto => 'udp',
+		sport => 53,
+		action => 'accept',
+	}
+}
+
+define iptables::hole ($proto='tcp', $port, $source=undef) {
+	firewall { "100 input: $name":
+		chain => 'INPUT',
+		proto => $proto,
+		dport => $port,
+		source => $source,
+		action => 'accept',
+        }
 }
