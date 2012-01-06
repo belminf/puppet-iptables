4a8f75c5147710111ccb5e03a0485ab6895987c9
Belmin Fernandez <belminf@gmail.com>
2012-01-05 21:37:48 -0500

    force include of iptables module (no way around it?) and fixed fail2ban issue

--- manifests/persist.pp
+++ manifests/persist.pp
@@ -1,9 +1,12 @@
-$iptables_save = "${pre_iptables_save}iptables -P INPUT DROP && iptables -P OUTPUT ACCEPT && service iptables save${post_iptables_save}"
+include "iptables"
 
-if $fail2ban_installed == 'true' {
+if $fail2ban_installed {
 	$pre_iptables_save = 'service fail2ban stop &&'
 	$post_iptables_save = '&& service fail2ban start'
 }
+
+$iptables_save = "${pre_iptables_save}iptables -P INPUT DROP && iptables -P OUTPUT ACCEPT && service iptables save${post_iptables_save}"
+
 
 exec { 'iptables-persist':
 	path => '/sbin/',
